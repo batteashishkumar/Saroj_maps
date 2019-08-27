a50e82dd7411a44ebd2726941a9d162b79c1b088
batteashishkumar <ashishakash55@gmail.com>
2019-08-27 16:42:09 +0530

    commit

--- app/src/main/java/com/example/saroj_maps/MainActivity.java
+++ app/src/main/java/com/example/saroj_maps/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
         adapter=new List_adapter(this,location_strs);
         listView.setAdapter(adapter);
-
+//
     }
 
     @Override
